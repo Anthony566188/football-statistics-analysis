d309c25625adaad0090c4f8ca6ba5b577ac288c2
Anthony Souza <souzaanthony892@gmail.com>
2026-05-14 17:30:41 -0300

    removendo comentarios desnecessarios

--- models/game.py
+++ models/game.py
@@ -5,15 +5,15 @@ from pydantic import BaseModel, Field
 
 
 class Game(BaseModel):
-    date: Optional[date]# = None
+    date: Optional[date]
     home_team: str = Field(..., min_length=1)
     away_team: str = Field(..., min_length=1)
-    home_goals: Optional[int]# = None
-    away_goals: Optional[int]# = None
-    yellow_cards_home: Optional[int]# = None
-    yellow_cards_away: Optional[int]# = None
-    home_fouls: Optional[int]# = None
-    away_fouls: Optional[int]# = None
-    home_corners: Optional[int]# = None
-    away_corners: Optional[int]# = None
+    home_goals: Optional[int]
+    away_goals: Optional[int]
+    yellow_cards_home: Optional[int]
+    yellow_cards_away: Optional[int]
+    home_fouls: Optional[int]
+    away_fouls: Optional[int]
+    home_corners: Optional[int]
+    away_corners: Optional[int]
 
